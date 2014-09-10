fffddce9fbcc0ebde1a4ac8389552f0568be4027
uyenbui14 <wendybui@gmail.com>
2014-09-09 21:25:42 -0700

    week3lab4

---
+++ Java_class/src/week3lab4.java
@@ -0,0 +1,11 @@
+
+public class week3lab4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number1 = "Hello";
+		String number2 = "World";
+		System.out.printf (number1 + " " + number2);
+	}
+
+}
