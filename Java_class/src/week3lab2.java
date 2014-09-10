c4937101eb1f00a503415d0b7718be4f0eff118b
uyenbui14 <wendybui@gmail.com>
2014-09-09 20:17:12 -0700

    week2 lab 3 check in

---
+++ Java_class/src/week3lab2.java
@@ -0,0 +1,11 @@
+
+public class week3lab2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(" Hello");
+		System.out.println(" My name is Uyen Bui");
+
+	}
+
+}
