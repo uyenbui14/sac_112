cdd0f77d780e1320783d07f7f2ab46cfe7ba325c
uyenbui14 <wendybui@gmail.com>
2014-09-09 19:44:03 -0700

    check in my name

---
+++ Java_class/src/myname.java
@@ -0,0 +1,10 @@
+
+public class myname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello");
+		System.out.println(" My name is Uyen Bui");
+	}
+
+}
